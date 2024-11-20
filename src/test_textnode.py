17ecd572c351b78804e5b278f5a12c1fbb5b14f1
Zelieen <firina@gmx.net>
2024-11-20 12:00:12 +0100

    add text_to_html func

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -33,6 +33,30 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.CODE)
         self.assertNotEqual(node, node2)
 
+    def test_convert_text(self):
+        test_text = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(repr(text_node_to_html_node(test_text)), f"LeafNode(None, This is a text node, None)")
+    
+    def test_convert_bold(self):
+        test_text = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        self.assertEqual(repr(text_node_to_html_node(test_text)), f"LeafNode(b, This is a text node, None)")
+
+    def test_convert_ital(self):
+        test_text = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        self.assertEqual((text_node_to_html_node(test_text)).to_html(), "<i>This is a text node</i>")
+
+    def test_convert_link(self):
+        test_text = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
+        self.assertEqual((text_node_to_html_node(test_text)).to_html(), '<a href="https://www.boot.dev">This is a text node</a>')
+
+    def test_convert_image(self):
+        test_text = TextNode("This is a text node", TextType.IMAGE, "https://www.boot.dev")
+        self.assertEqual((text_node_to_html_node(test_text)).to_html(), '<img src="https://www.boot.dev" alt="This is a text node"></img>')
+
+    def test_convert_error(self):
+        test_text = TextNode("This is a text node", TextType.ERROR, "https://www.boot.dev")
+        with self.assertRaisesRegex(Exception, "Not a valid text type: TextType.ERROR"):
+            (text_node_to_html_node(test_text))   
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -7,6 +8,7 @@ class TextType(Enum):
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
+    ERROR = "ERROR"
 
 class TextNode:
     def __init__(self, text, type, url=None):
@@ -19,3 +21,23 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if isinstance(text_node, TextNode) == False:
+        raise ValueError("Failed. Not a proper text node")
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)        
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        
+        case _:
+            raise Exception(f"Not a valid text type: {text_node.text_type}")
